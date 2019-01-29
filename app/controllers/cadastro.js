a306640b4866e1b3f8d512a8ab878cf96504f927
YohanaPinheiro <46759060+YohanaPinheiro@users.noreply.github.com>
2019-01-29 16:41:53 -0200

    Add files via upload

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,23 +1,34 @@
-module.exports.cadastro = function(application, req, res){
-    res.render('cadastro', { validacao: {}, dadosForm: {} });
+module.exports.cadastro = function (application, req, res){
+	res.render('cadastro', {validacao: {}, dadosForm: {}});
+}
+
+module.exports.cadastrar_novo_usuario = function(application, req, res){
+	res.render('/cadastro', {validacao: {}});
 }
 
 module.exports.cadastrar = function(application, req, res){
-    
-    var dadosForm = req.body;
 
-    req.assert('nome', 'Nome não pode ser vazio').notEmpty();
-    req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
-    req.assert('senha', 'Senha não pode ser vazia').notEmpty();
-    req.assert('casa', 'Casa não pode ser vazia').notEmpty();
+	var dadosForm = req.body;
 
-    var erros = req.validationErrors();
+	req.assert('nome', 'Nome não pode ser vazio').notEmpty();
+	req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+	req.assert('senha', 'Senha não pode ser vazio').notEmpty();
+	req.assert('casa', 'Casa não pode ser vazio').notEmpty();
 
-    if(erros){
-        res.render('cadastro', { validacao: erros, dadosForm: dadosForm });
-        return;
-    }
+	var erros = req.validationErrors();
 
-    res.send('podemos cadastrar');
+	if(erros){
+		res.render('cadastro', {validacao: erros, dadosForm: dadosForm});
+		return;
+	}
 
+	var connection = application.config.dbConnection;
+	var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+	var JogoDAO = new application.app.models.JogoDAO(connection);
+
+	UsuariosDAO.inserirUsuario(dadosForm);
+	JogoDAO.gerarParametros(dadosForm.usuario);
+	//geração dos parametros
+
+	res.send('podemos cadastrar')
 }
--- app/routes/cadastro.js
+++ app/routes/cadastro.js
@@ -1,9 +1,13 @@
 module.exports = function(application){
 	application.get('/cadastro', function(req, res){
-		application.app.controllers.cadastro.cadastro(application,req,res);
+		application.app.controllers.cadastro.cadastro(application, req, res);
 	});
 
 	application.post('/cadastrar', function(req, res){
-		application.app.controllers.cadastro.cadastrar(application,req,res);
+		application.app.controllers.cadastro.cadastrar(application, req, res);
+	});
+
+	application.get('/cadastrar_novo_usuario', function(req, res){
+		application.app.controllers.cadastro.cadastrar_novo_usuario(application, req, res);
 	});
 }
